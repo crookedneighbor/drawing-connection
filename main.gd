3ae1003a9686e1f42ad996084f7566acc3eab9fd
Blade Barringer <blade@crookedneighbor.com>
2026-05-24 11:36:15 -0500

    refactor: remove melody player

--- main.gd
+++ main.gd
@@ -1,6 +1,5 @@
 class_name Main extends Node2D
 
-@onready var melody_player: MelodyPlayer = $MelodyPlayer
 @onready var pad: Pad = $Pad
 @onready var pencil: Pencil = $Pencil
 @onready var controls: Controls = $Controls
@@ -14,7 +13,6 @@ var is_drawing: bool:
 var current_note: String = ""
 
 func _ready() -> void:
-	pad.section_change.connect(_on_section_change)
 	pad.enter_drawable_area.connect(_on_enter_drawable_area)
 	pad.exit_drawable_area.connect(_on_exit_drawable_area)
 	pencil.released.connect(_on_released)
@@ -26,32 +24,21 @@ func _ready() -> void:
 		if c is Obstacle:
 			c.collide.connect(_on_collide)
 
-func _process(_delta: float) -> void:
-	if is_drawing:
-		melody_player.play_note(current_note)
-
 func restart() -> void:
 	get_tree().reload_current_scene()
 
-func _on_section_change(index: int) -> void:
-	current_note = melody_player.notes.keys()[index]
 
 func _on_released() -> void:
 	pressing = false
-	melody_player.volume_db = -80
 
 func _on_enter_drawable_area() -> void:
-	if pressing:
-		melody_player.volume_db = 0
 	within_bounds = true
 
 func _on_exit_drawable_area() -> void:
-	melody_player.volume_db = -80
 	within_bounds = false
 
 func _on_start() -> void:
 	pencil.start()
-	melody_player.volume_db = 0
 	pressing = true
 
 func _on_collide() -> void:
@@ -60,7 +47,6 @@ func _on_collide() -> void:
 
 func _on_win() -> void:
 	pencil.stop()
-	melody_player.volume_db = -80
 	print("TODO - load win screen")
 
 func _on_restart() -> void:
--- tools/pad/pad.gd
+++ tools/pad/pad.gd
@@ -6,23 +6,12 @@ signal section_change
 
 @export_range(1, 20) var number_of_horizontal_areas: int = 1
 
-@onready var horizontal_areas: Node2D = $HorizontalAreas
 @onready var drawable_area: Area2D = $Area2D
 @onready var drawable_area_collision: CollisionShape2D = $Area2D/CollisionShape2D
 
 func _ready() -> void:
 	drawable_area.mouse_entered.connect(_on_mouse_enter)
 	drawable_area.mouse_exited.connect(_on_mouse_exit)
-	
-	var position_of_drawable_area = drawable_area_collision.position
-	var size = drawable_area_collision.shape.get_rect().size
-	var width_of_horizontal_areas = size.x
-	var height_of_horizontal_areas= size.y / number_of_horizontal_areas
-	for i in number_of_horizontal_areas:
-		var section = PadSection.new(i, width_of_horizontal_areas, height_of_horizontal_areas)
-		section.set_pos(position_of_drawable_area.x, position_of_drawable_area.y + (i * height_of_horizontal_areas))
-		section.mouse_entered.connect(func(): section_change.emit(section.index))
-		horizontal_areas.add_child(section)
 
 func _on_mouse_enter() -> void:
 	enter_drawable_area.emit()
