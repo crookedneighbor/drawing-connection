afcc9a4d5c3e281446cb486d70461892768eac91
Blade Barringer <blade@crookedneighbor.com>
2026-05-25 14:58:51 -0500

    feat: add speech bubble

--- obstacles/speech_bubble/speech_bubble.gd
+++ obstacles/speech_bubble/speech_bubble.gd
@@ -1,8 +1,15 @@
 class_name SpeechBubble extends Obstacle
 
-@onready var label: RichTextLabel = $RichTextLabel
+@onready var label: RichTextLabel = $Sprite2D/RichTextLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var reveal_area: Area2D = $"Reveal Area"
 
 @export var text: String
 
 func _ready() -> void:
 	label.text = text
+	reveal_area.area_entered.connect(_on_close_to_speech)
+
+func _on_close_to_speech(area: Area2D) -> void:
+	if area.get_parent() is DrawnLine:
+		animation_player.play("reveal")
